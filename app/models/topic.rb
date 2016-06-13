482960ef33e6b58624425a9c302863e5a70fa0d8
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2016-06-12 21:23:14 -0400

    added search to topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,12 @@
 class Topic < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, :uniqueness => {:case_sensitive => false}
+
+  def as_json(options={})
+    super(:only => [:id, :name, :description]
+    )
+  end
+
+  def self.search(search)
+    Topic.where("name LIKE ? OR description LIKE ?", "%#{search}%", "%#{search}%").distinct
+  end
 end
--- app/models/topic_item.rb
+++ app/models/topic_item.rb
@@ -1,5 +1,16 @@
 class TopicItem < ActiveRecord::Base
-  belongs_to :topic
+  belongs_to :topic, :autosave => true
   belongs_to :topicable, polymorphic: true
   accepts_nested_attributes_for :topic
+  before_validation :check_for_existing_topic
+
+  def check_for_existing_topic
+    # Find or create the topic by name
+    if new_topic = Topic.find_by_name(topic.name)
+      self.topic = new_topic
+    else
+      self.topic.save!
+    end
+    Rails.logger.debug topic.inspect
+  end
 end
